37341660ef795523f033c207821f4aa4b228a9bb
Ben <2551337+ben-alexander@users.noreply.github.com>
2026-01-24 13:16:16 +0000

    Add Latin numerals

--- numberology/systems/__init__.py
+++ numberology/systems/__init__.py
@@ -1,5 +1,6 @@
 from ..systems.arabic import Arabic
 from ..systems.egyptian import Egyptian
+from ..systems.latin import Latin
 from ..systems.roman import Roman
 
-__all__ = ["Arabic", "Egyptian", "Roman"]
+__all__ = ["Arabic", "Egyptian", "Latin", "Roman"]
